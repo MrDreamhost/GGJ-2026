3ad9fc6ef4b4bc7282ad3fa5a47189b2f537623f
MrDreamhost <r.wilmer@live.nl>
2026-01-30 23:05:29 +0100

    basic audio setup

--- Scripts/PlayerCharacter/PlayerCharacter.cs
+++ Scripts/PlayerCharacter/PlayerCharacter.cs
@@ -3,93 +3,93 @@ using System;
 
 public partial class PlayerCharacter : CharacterBody2D
 {
-    [Export] private int baseSpeed = 300;
-    [Export] private int baseAcceleration = 50;
-    [Export] private Area2D collider = null;
+	[Export] private int baseSpeed = 300;
+	[Export] private int baseAcceleration = 50;
+	[Export] private Area2D collider = null;
 
-    private State currentState = State.EIdle;
-    
-    public enum State{EIdle, EWalk, ETalking}
+	private State currentState = State.EIdle;
+	
+	public enum State{EIdle, EWalk, ETalking}
 
-    public State GetState()
-    {
-        return currentState;
-    }
+	public State GetState()
+	{
+		return currentState;
+	}
 
-    public override void _Ready()
-    {
-        if (collider == null)
-        {
-            Logger.Fatal("Area2D collider not assigned on playerCharacter");
-        }
-        base._Ready();
-    }
+	public override void _Ready()
+	{
+		if (collider == null)
+		{
+			Logger.Fatal("Area2D collider not assigned on playerCharacter");
+		}
+		base._Ready();
+	}
 
-    public override void _PhysicsProcess(double delta)
-    {
-        ProcessInputs();
-        SetVelocity(new Vector2(ProcessHorizontalInput(), ProcessVerticalInput()));
-        UpdateState();
-        MoveAndSlide();
-        base._PhysicsProcess(delta);
-    }
+	public override void _PhysicsProcess(double delta)
+	{
+		ProcessInputs();
+		SetVelocity(new Vector2(ProcessHorizontalInput(), ProcessVerticalInput()));
+		UpdateState();
+		MoveAndSlide();
+		base._PhysicsProcess(delta);
+	}
 
-    private void ProcessInputs()
-    {
-        if (Input.IsActionJustReleased("on_interact"))
-        {
-            var areas = collider.GetOverlappingAreas();
-            Logger.Info("Overlapping areas {0}", areas.Count);
-            foreach (var area in areas)
-            {
-                if (area is Interactable interactable)
-                {
-                  interactable.OnInteract(this);
-                }
-            }
-        }
+	private void ProcessInputs()
+	{
+		if (Input.IsActionJustReleased("on_interact"))
+		{
+			var areas = collider.GetOverlappingAreas();
+			Logger.Info("Overlapping areas {0}", areas.Count);
+			foreach (var area in areas)
+			{
+				if (area is Interactable interactable)
+				{
+				  interactable.OnInteract(this);
+				}
+			}
+		}
 
-        if (Input.IsActionJustReleased("progress_dialogue"))
-        {
-            UiManager.Instance.ContinueDialogueSequence();
-        }
-    }
-    
-    private void UpdateState()
-    {
-        switch (currentState)
-        {
-            case State.EIdle:
-                if (Velocity.X != 0)
-                    currentState = State.EWalk;
-                break;
-            case State.EWalk:
-                if (Velocity.X == 0)
-                    currentState = State.EIdle;
-                break;
-        }
-    }
+		if (Input.IsActionJustReleased("progress_dialogue"))
+		{
+			UiManager.Instance.ContinueDialogueSequence();
+		}
+	}
+	
+	private void UpdateState()
+	{
+		switch (currentState)
+		{
+			case State.EIdle:
+				if (Velocity.X != 0)
+					currentState = State.EWalk;
+				break;
+			case State.EWalk:
+				if (Velocity.X == 0)
+					currentState = State.EIdle;
+				break;
+		}
+	}
 
-    private float ProcessHorizontalInput()
-    {
-        var direction = 0;
-        if (Input.IsActionPressed("move_left"))
-            direction -= 1;
-        if (Input.IsActionPressed("move_right"))
-            direction += 1;
+	private float ProcessHorizontalInput()
+	{
+		var direction = 0;
+		if (Input.IsActionPressed("move_left"))
+			direction -= 1;
+		if (Input.IsActionPressed("move_right"))
+			direction += 1;
 
-        return Mathf.MoveToward(Velocity.X, direction * baseSpeed, baseAcceleration);
-    }
+		return Mathf.MoveToward(Velocity.X, direction * baseSpeed, baseAcceleration);
+	}
 
-    private float ProcessVerticalInput()
-    {
-        var direction = 0;
-        if (Input.IsActionPressed("move_up"))
-            direction -= 1;
-        if (Input.IsActionPressed("move_down"))
-            direction += 1;
+	private float ProcessVerticalInput()
+	{
+		var direction = 0;
+		if (Input.IsActionPressed("move_up"))
+			direction -= 1;
+		if (Input.IsActionPressed("move_down"))
+			direction += 1;
 
-        return Mathf.MoveToward(Velocity.Y, direction * baseSpeed, baseAcceleration);
-    }
-    
+		return Mathf.MoveToward(Velocity.Y, direction * baseSpeed, baseAcceleration);
+	}
+	
 }
