f0fc15cbf1246c17033c6e547bf9c8174827645c
MrDreamhost <r.wilmer@live.nl>
2026-02-01 00:58:19 +0100

    Finalised Main Scene

--- Scripts/World/NPC/NpcBase.cs
+++ Scripts/World/NPC/NpcBase.cs
@@ -1,24 +1,24 @@
-﻿using Godot;
+using Godot;
 using System;
 
 public partial class NpcBase : Interactable
 {
-    [Export] private int dialogueSequence;
+	[Export] private int dialogueSequence;
 
-    public override void _Ready()
-    {
-        if (dialogueSequence == 0)
-        {
-            Logger.DebugError("No dialogue sequence assigned to character");
-        }
+	public override void _Ready()
+	{
+		if (dialogueSequence == 0)
+		{
+			Logger.DebugError("No dialogue sequence assigned to character");
+		}
 
-        base._Ready();
-    }
+		base._Ready();
+	}
 
-    public override void OnInteract(PlayerCharacter playerCharacter)
-    {
-        Logger.Info("Interacted with NPC");
-        UiManager.Instance.StartDialogueSequence(dialogueSequence);
-        base.OnInteract(playerCharacter);
-    }
+	public override void OnInteract(PlayerCharacter playerCharacter)
+	{
+		Logger.Info("Interacted with NPC");
+		UiManager.Instance.StartDialogueSequence(dialogueSequence);
+		base.OnInteract(playerCharacter);
+	}
 }
