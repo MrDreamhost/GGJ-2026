ddf02ab3e7c5ea025c2933fa2e99cbb0c02fa575
Dice <thijsbouwhuis@outlook.com>
2026-01-31 14:25:10 +0100

    Added audio for interact

--- Scripts/World/Interactable.cs
+++ Scripts/World/Interactable.cs
@@ -6,7 +6,18 @@ using Godot.Collections;
 public partial class Interactable : Area2D
 {
     //All required flags to show up in-game
-    [Export]private Array<PlayerFlag> requiredFlags = new Array<PlayerFlag>();
+    [Export] private Array<PlayerFlag> requiredFlags = new Array<PlayerFlag>();
+    [Export] private AudioStreamPlayer2D audioStreamPlayer;
+
+    public override void _Ready()
+    {
+        if (audioStreamPlayer == null)
+        {
+            Logger.Fatal("AudioStreamPlayer is not assigned on Interactable");
+        }
+
+        base._Ready();
+    }
 
     //TODO RIP performance, can probably be on a 1sec timer
     public override void _Process(double delta)
@@ -22,17 +33,37 @@ public partial class Interactable : Area2D
                 Monitoring = false;
                 return;
             }
-            
+
             //otherwise we make it show
             Show();
             Monitorable = true;
             Monitoring = true;
         }
+
         base._Process(delta);
     }
 
     public virtual void OnInteract(PlayerCharacter playerCharacter)
     {
-        
+        audioStreamPlayer.Play();
+    }
+
+    protected virtual void DoHide()
+    {
+        this.Monitorable = false;
+        this.Monitoring = false;
+        this.Hide();
+        foreach (var child in GetChildren())
+        {
+            if (child is CanvasItem canvasChild)
+            {
+                canvasChild.Visible = false;
+            }
+
+            if (child is CollisionShape2D collisionShape2D)
+            {
+                collisionShape2D.Disabled = true;
+            }
+        }
     }
 }
--- Scripts/World/ItemInteractable.cs
+++ Scripts/World/ItemInteractable.cs
@@ -14,6 +14,6 @@ public partial class ItemInteractable : Interactable
         }
         playerCharacter.GetInventory().AddItem(itemId, amount, "Picked up from Interactable");
         base.OnInteract(playerCharacter);
-        QueueFree();
+        DoHide();
     }
 }
